3a17646824074cbadb210160ed23feeb8f7d7055
Kim <kimia.gholizadeh@pongstudios.com>
2025-09-24 16:52:22 -0400

    Delete AIQA directory

--- AIQA/ui/main_app.py
+++
@@ -1,8 +0,0 @@
-
-
-import streamlit as st
-st.set_page_config(page_title="AI Game Tester", layout="centered")
-st.title("AI QA ")
-st.write("Pick a product to test:")
-st.page_link("pages/1_edgelabs_gen2.py", label=" EdgeLabs Gen 2")
-st.caption("Add more products by creating more files in ui/pages/.")
